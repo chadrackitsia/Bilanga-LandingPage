ee3a4a501b4a975cc2dc537b25cf06e0bebe45bc
Chadrack Itsia <itsia001@gmail.com>
2020-03-23 02:14:21 +0100

    Aujout de la fonction hiddenMenu()

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,10 @@
+// HIDDEN MENU
+function hiddenMenu(){
+    document.querySelector('.navbar').classList.add('afficheMenu');
+}
+
+setTimeout(hiddenMenu, 3000);
+
 // LOADER - DOM JS
 function showContent() {
     document.querySelector('.loader-container').classList.add('hidden');
